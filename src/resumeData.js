a86d3df005f853c04469053655c51c2e693bb36c
Maria Navarrete <marianavar12@gmail.com>
2019-05-14 12:25:51 -0400

    port rep

---
+++ src/resumeData.js
@@ -0,0 +1,112 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Maria",
+  "role": "Nerds can Fashion | Scrum Master | Full Stack Web Developer | Project Manager",
+  "linkedinId":"https://www.linkedin.com/in/marianavar/",
+  "roleDescription": " ",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/in/marianavar/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"https://github.com/marianavar",
+        "className":"fa fa-github"
+      },
+
+    ],
+  "aboutme":"I was born in Venezuela but moved to the United States at the age of 13. Throughout my career I have vastly increased my knowledge in business, fashion, and technology. Growing up, I pursued the value of style and trends, while focusing on customer service; I have been able to expand my horizons in different areas of work. Web development has come to me as a plus. With the dynamics of the changing world regarding e-commerce, my love for tech has also evolved. During my education, I received my MBA in Web Development from Lynn University in just 5 years having also double majored in International Business and Fashion and Retail. While at Wyncode I received training in a variety of tech skills. I created dynamic and responsive web applications with my team using Agile methodologies. While at Bloomingdales, I had the privilege to be trained by my manager on how to manage our team because he knew how motivated I was to be a team leader. I’m working diligently to bring my love for coding, design, and fashion into the business world.",
+  "address":"Weston, FL, USA",
+  "website":"marianavar12@gmail.com",
+  "education":[
+    {
+      "UniversityName":"Lynn University",
+      "specialization":"MBA in Web Development with a specialization in International Business",
+      "MonthOfPassing":"March",
+      "YearOfPassing":"2019",
+      "Achievements":"Graduated with 3.9 GPA | Full Stack Web Developer certification with Wyncode Academy"
+    },
+    {
+      "UniversityName":"Lynn University",
+      "specialization":"Bachelor of Arts with a degree in International Business and a minor in Fashion and Retail",
+      "MonthOfPassing":"December",
+      "YearOfPassing":"2017",
+      "Achievements":"Graduated Summa Cum Laude with a 3.9 GPA | Elected Vice President of the Animal Welfare Club from 2015-2018"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Miki Mau Music",
+      "specialization":"Intern/Project Manager Volunteer",
+      "Achievements":["Actively listened to community recommendations to schedule and organize camp activities within the Burning Man festival regulations. Designed wrist-bands, shirts, socks and postcards for the camp at Burning Man Festival, and launched a successful marketing campaign using Facebook adverts, Instagram, and word of mouth advertising to raise over $20,000 for various organizations."]
+    },
+    {
+      "CompanyName":"Bloomingdales",
+      "specialization":"Sales Associate",
+      "Achievements":"Trained store employees on sales, store operations, cashier transactions and organizing inventory. Applied effective project leadership skills, coaching, project pacing and control within coworkers to keep a tranquil work environment, and satisfied customer needs by learning and understanding their style, providing great service with the understanding of in-store inventory."
+    },
+    {
+      "CompanyName":"Raw Juce",
+      "specialization":"Team Member",
+      "Achievements":"Successfully trained new hires for the assembly line, POS, and back-stock. Effectively managed the POS system for the store to increase productivity, and Kept work areas clean, organized and safe to promote efficiency and team safety."
+    },
+    {
+      "CompanyName":"High Point University",
+      "specialization":"Spanish Tutor",
+      "Achievements":"Used creative ideas to teach students from classes levels one through four, as well as pronunciation. Supported students in weak areas and grade assignments as required.Kept accurate record of students and their progress reports."
+    },
+  ],
+  "skillsDescription":"Languages, Frameworks, & Libraries",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"JavaScript"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Ruby"
+    },
+    {
+      "skillname":"Rails"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"Gainz",
+      "description":"Web Application, Final Project at Wyncode. A daily workout-log, where the user can track measurements as well as workouts to later view the progress with the use of graphs and a calendar",
+      "imgurl":"images/portfolio/Gainz.png",
+      "url":"http://gainz-wyncode.herokuapp.com/"
+    },
+    {
+      "name":"brewfinder",
+      "description":"Web Application, simple brewery finder made for people to more easily locate breweries nation wide. Created with MapboxGL API and Open Brewery Finder DB API.",
+      "imgurl":"images/portfolio/brew2.jpg",
+      "url":"https://brewfinder.netlify.com/"
+    },
+  ],
+  "certificates":[
+    {
+      "CertificateName":"Professional Scrum Master I (PSM I)",
+      "location":"Scrum.org",
+      "MonthOfPassing":"April",
+      "YearOfPassing":"2019",
+    },
+    {
+      "CertificateName":"Full Stack Web Developer",
+      "location":"Wyncode Academy",
+      "MonthOfPassing":"March",
+      "YearOfPassing":"2019",
+    },
+  ],
+}
+
+export default resumeData
